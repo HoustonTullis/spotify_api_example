9d084fc6c2fa30a5c7c66c6a40601b1786585886
HoustonTullis <78330525+HoustonTullis@users.noreply.github.com>
2021-03-23 22:08:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -33,6 +33,26 @@ $( document ).ready(function() {
       window.location.replace(redirect);
     }
 
+    /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
     // Search button has been clicked
     $( "#search_button" ).click(function() {
       //Get the value of the search box
